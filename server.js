ffa2cb9ba15f19e5ef18dafa24f0f08d27a3b48f
Antonios ZALONIS <antonios.zalonis@ext.ec.europa.eu>
2025-03-18 11:22:25 +0000

    multiple photos scenes

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-// Serve static files (for images & HTML)
+// ✅ Serve static files from "public" folder
 app.use(express.static(path.join(__dirname, "public")));
 
+// ✅ Ensure Express serves all files inside "public/libs/"
+app.use("/libs", express.static(path.join(__dirname, "public/libs")));
+
+// ✅ Serve index.html
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
