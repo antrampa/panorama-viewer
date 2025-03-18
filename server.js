adcfe080467733f17cc871eca8d013bb50e256ad
Antonios ZALONIS <antonios.zalonis@ext.ec.europa.eu>
2025-03-18 10:35:40 +0000

    panoramic view initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+// Serve static files (for images & HTML)
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
